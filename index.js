5e8a646c49b06225301da233dc7730b55cfde357
Evan Solomon <evan@evanalyze.com>
2013-08-27 19:53:58 -0700

    And now your Docco docs are JSDocs!

--- index.js
+++ index.js
@@ -1,53 +1,39 @@
 // Copyright 2013 The Obvious Corporation.
 
-// ## S3 Utils
-//
-// Arguments:
-//
-// * `s3`: Authenticated `AWS.S3` instance
-//
-// Usage:
-//
-// ```
-// var AWS = require('aws-sdk');
-// var s3 = new AWS.S3(configParams);
-// var s3Utils = new S3Utils(s3);
-// ```
+var S3Stream = require('./lib/Stream');
 
+/**
+ * Helper functionality for working with S3.
+ *
+ * Usage:
+ *   var AWS = require('aws-sdk');
+ *   var s3 = new AWS.S3(configParams);
+ *   var s3Utils = new S3Utils(s3);
+ *
+ * @constructor
+ * @param {Object} s3 Authenticated instance of AWS.S3
+ */
 function S3Utils(s3) {
   this.s3 = s3;
 }
 module.exports = S3Utils;
 
-// ## Load S3Stream
-//
-// Loads the base S3Stream class, which extends `stream.Writable`.
-var S3Stream = require('./lib/Stream');
 
-// ## CreateWriteStream
-//
-// Usage:
-//
-// ```
-// var s3stream = s3Utils.createWriteStream({
-//   Bucket: 'random-access-memories',
-//   Key: 'to-get-lucky.log'
-// });
-// fs.createReadStream('./for-good-fun.log').pipe(s3stream);
-// ```
-//
-// * `params`: same params object as `AWS.S3.createMultipartUpload()`
-// * `callback`: Optional, called with `(err, writeableStream)`
-//
-// A writeable stream will be immediately returned, but the stream will not
-// be ready yet. An upload ID must be retrieved from S3 before the stream
-// will be ready. You can handle this in a few ways:
-//
-// * Wait for the stream to emit a `writable` event
-// * Provide a callback, which will be called with `(err, writeableStream)`
-//   when the stream is ready
-// * Start writing immediately and respect `false` return values. This is how
-//   Node's `stream.pipe()` behaves
+/**
+ * Create a writable stream to upload an object to S3.
+ *
+ * Usage:
+ *   var s3Utils = new S3Utils(s3);
+ *   var s3stream = s3Utils.createWriteStream({
+ *     Bucket: 'random-access-memories',
+ *     Key: 'to-get-lucky.log'
+ *   });
+ *   fs.createReadStream('./for-good-fun.log').pipe(s3stream);
+ *
+ * @param {Object} params Params to create an instance of S3Stream
+ * @param {Function=} callback Called when the stream is ready.
+ * @return {Stream} Writable stream
+ */
 S3Utils.prototype.createWriteStream = function (params, callback) {
   var s3stream = new S3Stream(params, this.s3);
 
