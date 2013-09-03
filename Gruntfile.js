8f6551dd7c6f393693cf2b27c2d8fef4cf00ad28
Evan Solomon <evan@evanalyze.com>
2013-09-02 21:27:18 -0700

    Publish jsdoc on github pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,12 @@
 module.exports = function(grunt) {
+  var path = require('path');
+  var docsRoot = '.grunt/docs';
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.initConfig({
     watch: {
@@ -14,9 +19,31 @@ module.exports = function(grunt) {
         tasks: 'exec:test'
       }
     },
+    jsdoc: {
+      docs: {
+        src: ['./*.js', './lib/*.js'],
+        options: {
+          destination: docsRoot,
+          private: true
+        }
+      }
+    },
+    'gh-pages': {
+      docs: {
+        src: '**/*',
+        options: {
+          base: docsRoot
+        }
+      }
+    },
     exec: {
       test: {
         command: 'npm test'
+      },
+      docsIndex: {
+        // The default index page is mostly empty and confusing
+        // This sets it to use the main class' docs
+        command: 'cp ' + [path.join(docsRoot, 'S3Utils.html'), path.join(docsRoot, 'index.html')].join(' ')
       }
     },
     jshint: {
@@ -24,5 +51,6 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.registerTask('default', ['jshint', 'exec:test']);
+  grunt.registerTask('default', ['jshint', 'exec:test', 'docs']);
+  grunt.registerTask('docs', ['jsdoc:docs', 'exec:docsIndex']);
 };
